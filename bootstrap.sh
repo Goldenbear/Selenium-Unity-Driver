ac4191d05b4c2b0815761d99aba6c5be2d02ad04
gopokyo-colinn <colinn@gopokyo.com>
2016-08-17 11:36:21 -0300

    adding sudo to some commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-npm install -g node-inspector
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install rbenv ruby-build
+sudo npm install -g node-inspector
+sudo ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+sudo ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo brew install rbenv ruby-build
 echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
 source ~/.bash_profile
-rbenv install 2.3.1
-rbenv global 2.3.1
+sudo rbenv install 2.3.1
+sudo rbenv global 2.3.1
 ruby -v
-brew install --HEAD ideviceinstaller
+sudo brew install --HEAD ideviceinstaller
 sudo gem install cocoapods
 pod setup
 cd Appium-Dot-App
