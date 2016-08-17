a3744b4bd26c51ba1c030bef89e025d40ff1f880
Colin Nickerson <colinn@gopokyo.com>
2016-08-17 19:00:02 -0300

    bug fix and formatting

--- Appium-Unity/Assets/source/HCP/Requests/GetElementAttributeRequest.cs
+++ Appium-Unity/Assets/source/HCP/Requests/GetElementAttributeRequest.cs
@@ -13,7 +13,7 @@ namespace HCP.Requests
         public enum EAttribute
         {
             NAME,
-			CLASSNAME,
+            CLASSNAME,
             DISPLAYED,
             ENABLED,
             SELECTED
@@ -26,94 +26,94 @@ namespace HCP.Requests
             {
                 var stringValue = Data["attribute"];
 
-                switch(stringValue)
-				{
-					case "name":   		return EAttribute.NAME;
-					case "className":   return EAttribute.CLASSNAME;
-                    case "displayed":   return EAttribute.DISPLAYED;
-                    case "enabled":     return EAttribute.ENABLED;
-                    case "selected":    return EAttribute.SELECTED;
+                switch (stringValue)
+                {
+                    case "name": return EAttribute.NAME;
+                    case "className": return EAttribute.CLASSNAME;
+                    case "displayed": return EAttribute.DISPLAYED;
+                    case "enabled": return EAttribute.ENABLED;
+                    case "selected": return EAttribute.SELECTED;
 
                     default: throw new FormatException("Unsupported element request");
                 }
             }
         }
 
-        
+
         public GetElementAttributeRequest(JSONClass json) : base(json)
         {
         }
 
-		public static string GetClassName(Element element)
-		{
-			var textComponent = element.GetComponent<UnityEngine.UI.Text>();
-			var canvasComponent = element.GetComponent<UnityEngine.Canvas>();
-			var buttonComponent = element.GetComponent<UnityEngine.UI.Button>();
-			var toggleComponent = element.GetComponent<UnityEngine.UI.Toggle>();
-			var dropdownComponent = element.GetComponent<UnityEngine.UI.Dropdown>();
-			var inputComponent = element.GetComponent<UnityEngine.UI.InputField();
+        public static string GetClassName(Element element)
+        {
+            var textComponent = element.GetComponent<UnityEngine.UI.Text>();
+            var canvasComponent = element.GetComponent<UnityEngine.Canvas>();
+            var buttonComponent = element.GetComponent<UnityEngine.UI.Button>();
+            var toggleComponent = element.GetComponent<UnityEngine.UI.Toggle>();
+            var dropdownComponent = element.GetComponent<UnityEngine.UI.Dropdown>();
+            var inputComponent = element.GetComponent <UnityEngine.UI.InputField>();
 
 
 
 
-			if (canvasComponent != null) 
-			{
-				return (canvasComponent.GetType ().FullName);
-			} 
-			else if (textComponent != null) 
-			{
-				return (textComponent.GetType ().FullName);
-			} 
-			else if (buttonComponent != null) 
-			{
-				return (buttonComponent.GetType ().FullName);
-			} 
-			else if (toggleComponent != null) 
-			{
-				return (toggleComponent.GetType ().FullName);
-			}
-			else if (dropdownComponent != null) 
-			{
-				return (dropdownComponent.GetType ().FullName);
-			} 
-			else if (inputComponent != null) 
-			{
-				return (inputComponent.GetType ().FullName);
-			} 
-			else 
-			{
-				return (element.GetType ().FullName);
-			}
-		}
+            if (canvasComponent != null)
+            {
+                return (canvasComponent.GetType().FullName);
+            }
+            else if (textComponent != null)
+            {
+                return (textComponent.GetType().FullName);
+            }
+            else if (buttonComponent != null)
+            {
+                return (buttonComponent.GetType().FullName);
+            }
+            else if (toggleComponent != null)
+            {
+                return (toggleComponent.GetType().FullName);
+            }
+            else if (dropdownComponent != null)
+            {
+                return (dropdownComponent.GetType().FullName);
+            }
+            else if (inputComponent != null)
+            {
+                return (inputComponent.GetType().FullName);
+            }
+            else
+            {
+                return (element.GetType().FullName);
+            }
+        }
 
         public override JobResponse Process()
         {
             string response = null;
             var element = JobRequest.GetElementById(this.Id);
 
-            switch(this.Attribute)
-			{
-				case EAttribute.NAME:
-					response = element.gameObject.name;
-					break;
-				case EAttribute.CLASSNAME:
-					response = GetClassName(element);
-					break;
+            switch (this.Attribute)
+            {
+                case EAttribute.NAME:
+                    response = element.gameObject.name;
+                    break;
+                case EAttribute.CLASSNAME:
+                    response = GetClassName(element);
+                    break;
                 case EAttribute.DISPLAYED:
-					{
-						UnityEngine.Vector3 point = GetElementLocationRequest.GetLocation(element);
-						UnityEngine.Vector3 size = GetElementSizeRequest.GetSize(element);
+                    {
+                        UnityEngine.Vector3 point = GetElementLocationRequest.GetLocation(element);
+                        UnityEngine.Vector3 size = GetElementSizeRequest.GetSize(element);
 
-						if (point.y < 0 || point.y+size.y > UnityEngine.Screen.height ||
-							point.x < 0 || point.x+size.x > UnityEngine.Screen.width) 
-						{
-							response = "false";
-						} 
-						else
-						{
-							response = element.gameObject.activeInHierarchy ? "true" : "false";
-						}
-					}
+                        if (point.y < 0 || point.y + size.y > UnityEngine.Screen.height ||
+                            point.x < 0 || point.x + size.x > UnityEngine.Screen.width)
+                        {
+                            response = "false";
+                        }
+                        else
+                        {
+                            response = element.gameObject.activeInHierarchy ? "true" : "false";
+                        }
+                    }
                     break;
                 case EAttribute.ENABLED:
                     response = element.gameObject.activeSelf ? "true" : "false";
@@ -123,7 +123,7 @@ namespace HCP.Requests
                     break;
             }
 
-            return  new Responses.StringResponse(response);
+            return new Responses.StringResponse(response);
         }
     }
 }
