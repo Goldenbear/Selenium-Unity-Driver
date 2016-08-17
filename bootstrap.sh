6d702ed852136d274c0ffc1f2d9157b93d65f3b8
gopokyo-colinn <colinn@gopokyo.com>
2016-08-17 11:41:54 -0300

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 sudo npm install -g node-inspector
-sudo ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-sudo ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo brew install rbenv ruby-build
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+brew install rbenv ruby-build
 echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
 source ~/.bash_profile
-sudo rbenv install 2.3.1
-sudo rbenv global 2.3.1
+rbenv install 2.3.1
+rbenv global 2.3.1
 ruby -v
 sudo brew install --HEAD ideviceinstaller
 sudo gem install cocoapods
